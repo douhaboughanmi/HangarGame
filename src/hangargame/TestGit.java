4915bcfbf25b67f24d32597d0bc880110f143b75
louaydhyeb <louay.dhyeb@esprit.tn>
2017-02-03 23:44:47 +0100

    Test git ya wlad

---
+++ src/hangargame/TestGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangargame;
+
+/**
+ *
+ * @author Louay
+ */
+public class TestGit {
+    private String Louay;
+
+    public TestGit(String Louay) {
+        this.Louay = Louay;
+    }
+    
+}
