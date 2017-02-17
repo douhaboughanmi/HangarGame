cb6c0e5d3aea888d46a5fbebe911ac5d3e38d0cb
mishkasama <yesmine.abdennadher@esprit.tn>
2017-02-17 01:16:29 +0100

    login

--- src/hangargame/xml/AffichageAdminJeuController.java
+++ src/hangargame/xml/AffichageAdminJeuController.java
@@ -6,16 +6,20 @@
 package hangargame.xml;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextField;
 import hangargame.connexionDB.ConnexionSingleton;
 import hangargame.entites.Actualite;
 import hangargame.entites.JeuxVideo;
 import hangargame.services.CrudJeuxVideo;
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -27,9 +31,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.MouseEvent;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -57,19 +64,40 @@ public class AffichageAdminJeuController implements Initializable {
 
     @FXML
     private TableColumn<?, ?> descj;
+    
+   
 
-    @FXML
-    private TableColumn<?, ?> image;
+    
 
   
-    @FXML
-    private JFXButton btnAfficher= new JFXButton();
     @FXML
     private TableColumn<?, ?> colid;
     @FXML
     private JFXButton btnSup;
-
     @FXML
+    private JFXTextField titreJeu;
+    @FXML
+    private JFXTextField GenreJeu;
+    @FXML
+    private TextArea descriptionjeu;
+    @FXML
+    private JFXButton BtnImage;
+    @FXML
+    private JFXButton btnmodif;
+    int id ; 
+    @FXML
+    private JFXDatePicker dateJEu;
+    
+    
+    
+    
+    private ObservableList<JeuxVideo> data ;
+    @FXML
+    private JFXTextField rech;
+    @FXML
+    private JFXButton btnCher;
+   
+
     void afficher() {
         
         
@@ -78,7 +106,7 @@ public class AffichageAdminJeuController implements Initializable {
         genrejeu.setCellValueFactory(new PropertyValueFactory<>("genre"));
         descj .setCellValueFactory(new PropertyValueFactory<>("description"));
         datejeu.setCellValueFactory(new PropertyValueFactory<>("date_sortie"));
-        image.setCellValueFactory(new PropertyValueFactory<>("image"));
+     //  image.setCellValueFactory(new PropertyValueFactory<>("image"));
         
         
         tablejeu.setItems(null);
@@ -111,10 +139,111 @@ public class AffichageAdminJeuController implements Initializable {
       JeuxVideo j = new JeuxVideo();
         CrudJeuxVideo cj = new CrudJeuxVideo();
       cj.supprimerJeuxVideo(tablejeu.getSelectionModel().getSelectedItem().getNom());
-       //afficher();
+       afficher();
         
         
     }
+
+    @FXML
+    private void showcliked(MouseEvent event) {
+       
+        
+        JeuxVideo jeu = tablejeu.getSelectionModel().getSelectedItem();
+        jeu.toString();
+        id=jeu.getId();
+        System.out.println(id);
+        System.out.println(jeu.getNom());
+        System.out.println(jeu.getGenre());
+        titreJeu.setText(jeu.getNom());
+       GenreJeu.setText(jeu.getGenre());
+        descriptionjeu.setText(jeu.getDescription());
+          String datej= dateJEu.
+                getValue()
+                .format
+        (DateTimeFormatter.
+                ofPattern("YYYY-MM-DD"));    
+          
+        
+      //  image.setText(jeu.getImage());
+
+             
+        
+        // date a faire 
+        
+       // input=jeu.getInputStream();
+     //   ImageView imageView = new ImageView(new Image(annonces.getInputStream()));
+     //       imageView.setFitHeight(112);
+        //    imageView.setFitWidth(178);
+
+         //   imageMesAnnonces.setGraphic(imageView);
+    }
+
+    @FXML
+    private void selectImage(ActionEvent event) {
+        
+        
+        
+    }
+
+    @FXML
+    private void Modifier(ActionEvent event) {
+        
+        
+      String datej= dateJEu.
+                getValue()
+                .format
+        (DateTimeFormatter.
+                ofPattern("YYYY-MM-DD")); 
+      
+    String tj = titreJeu.getText();
+    String genrej =GenreJeu.getText();
+    String desj = descriptionjeu.getText();
+    JeuxVideo jeu=new JeuxVideo(id, tj, genrej,datej , desj,"", "");
     
+    crud.modifierJeuxVideo(jeu);
+    afficher();
+    
+//    if(crud.modifierJeuxVideo(jeu))
+//    {
+//        System.out.println("OOKK");
+//        data=FXCollections.observableArrayList();
+//     data.addAll(crud.reccuperer());
+//     afficher();
+//    }
+//    else
+//    {
+//        System.out.println("Non");
+//    }
+     System.out.println(genrej);
+    }
+   
+   
+   
+   // data=FXCollections.observableArrayList();
+
+    @FXML
+    private void rechercher(ActionEvent event) {
+         String nomJeu = rech.getText();
+         
+          nomjeu.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        genrejeu.setCellValueFactory(new PropertyValueFactory<>("genre"));
+        descj .setCellValueFactory(new PropertyValueFactory<>("description"));
+        datejeu.setCellValueFactory(new PropertyValueFactory<>("date_sortie"));
+     // image.setCellValueFactory(new PropertyValueFactory<>("image"));
+        
+        
+        tablejeu.setItems(null);
+        tablejeu.setItems(crud.rechercher(nomJeu));
+       
+        
+        
+    }
+
 }
+
+   
+
+
+    
+
 
